8aa47bb8dd60c26064b18e7560897d55646aeb44
Jaz Lim <angmeng@gmail.com>
2018-11-25 08:00:56 +0000

    Check the order machine object is valid?

--- app/controllers/orderings_controller.rb
+++ app/controllers/orderings_controller.rb
@@ -7,12 +7,14 @@ class OrderingsController < ApplicationController
   
   def order
     order_machine = OrderMachine.order(session[:order_id], session[:user_id], params[:menu_item_id],  params[:quantity] )
-                                     
-    if order_machine.valid? && order_machine.ordering
-      session[:order_id] = order_machine.order.id
-      flash[:notice] = "you have just order #{order_machine.menu_item.name}"
-    else
-      flash[:alert] = "You need a valid quantity"
+        
+    if order_machine.valid?                             
+      if order_machine.ordering
+        session[:order_id] = order_machine.order.id
+        flash[:notice] = "you have just order #{order_machine.menu_item.name}"
+      else
+        flash[:alert] = "You need a valid quantity"
+      end
     end
     
     redirect_to action: :index
