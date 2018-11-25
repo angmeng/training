530e41fed3d58ceaebafabf9e23bd6736e4e49f2
EC2 Default User <ec2-user@ip-172-31-7-147.us-east-2.compute.internal>
2018-11-25 09:00:30 +0000

    check order machine

--- app/controllers/orderings_controller.rb
+++ app/controllers/orderings_controller.rb
@@ -4,7 +4,7 @@ class OrderingsController < ApplicationController
   def index
     @categories = Category.order("name DESC")
   end
-  
+
   def order
     order_machine = OrderMachine.order(session[:order_id], session[:user_id], params[:menu_item_id],  params[:quantity] )
                                      
