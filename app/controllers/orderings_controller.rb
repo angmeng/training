1bdd8c89c3208c836438f97f4f873cc14f0a5dce
hong7775 <hong7775@gmail.com>
2018-11-25 08:44:08 +0000

    edit_ordering_controller

--- app/controllers/orderings_controller.rb
+++ app/controllers/orderings_controller.rb
@@ -9,6 +9,7 @@ class OrderingsController < ApplicationController
     order_machine = OrderMachine.order(session[:order_id], session[:user_id], params[:menu_item_id],  params[:quantity] )
                                      
     if order_machine.valid? && order_machine.ordering
+      
       session[:order_id] = order_machine.order.id
       flash[:notice] = "you have just order #{order_machine.menu_item.name}"
     else
